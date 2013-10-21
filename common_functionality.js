50bb0e4d83ea7e3b8c3a71cf4d595d0671da0416
eddy jones <ejones@estevenjones.com>
2013-10-21 08:03:43 -0600

    Initial Checking

---
+++ common_functionality.js
@@ -0,0 +1,19 @@
+function drawMenuBar(){
+	document.write("
+		<div class=\"container\">
+            <div class=\"row\">
+                <span class=\"MenuBlock SubPage col_11 bordered\">
+                <a href=\"/\" class=\"MenuItem\"> HOME&nbsp;</a>|&nbsp;
+                <a href=\"#\" class=\"MenuItem\">&nbsp;CONTACT&nbsp;</a>|&nbsp;
+                <a href=\"#\" class=\"MenuItem\">&nbsp;ABOUT ME&nbsp;</a>|&nbsp;
+                <a href=\"blog.html\" class=\"MenuItem\">&nbsp;BLOG&nbsp;</a>|&nbsp;
+                <a href=\"#\" class=\"MenuItem\">&nbsp;GALLERY&nbsp;</a>
+                <span class=\"TitleBlock\">
+                    <h1>Splinterworks</h1>
+                    <span class=\"subtitle\">&nbsp;Guitars&nbsp;for&nbsp;Guitarists</span>
+                </span>
+            </span>
+            </div>
+        </div>"
+	);
+}
